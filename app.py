62b4d49d7aacf6d19b2d830a2e02b8e389fa2385
boar1ang <inq.ops@gmail.com>
2020-02-21 18:33:57 -0600

    Fri. push

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import join
 import json
 import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
 
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///data/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
@@ -16,33 +18,39 @@ Base.classes.keys()
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-#Create Flask app:
-climate_app = Flask(__name__)
+# #Create Flask app:
+app = Flask(__name__)
 @app.route("/")
 def home():
     print("Server received request for Climate App 'Home' page.")
     return("Welcome to the Climate App Home page!<br/>"
            "This site provides access to an amazing collection of climate data from Hawaii.<br/>"
-           "Would you like to use our API? Available routes include:<br/>"
+           "Would you like to use our API? Available API routes include:<br/>"
            f"Precipitation: /api/v1.0/precipitation<br/>"
            f"Weather stations: /api/v1.0/stations<br/>"
            f"Temperature observations over 1 year: /api/v1.0/tobs<br/>" 
            f"Temperature observations from start date: /api/v1.0/tobs<br/>"
            f"Temperature from specified start date to specified end date: /api/v1.0/<start> and /api/v1.0/<start>/<end><br/>")
-            
+
 #### API route 1/5:
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
     sel = [Measurement.prcp, Measurement.date]
     result=session.query(*sel).all()
+    session.close()
+    
     precipitation = []
     for date, prcp in result:
         prcp_dict={}
         prcp_dict["precipitation"] = prcp
         prcp_dict["date"] = date
         precipitation.append(prcp_dict)
+        
+    precipitation = list(np.ravel(result))    
     return jsonify(precipitation)
+
+
     
 #### API route 2/5:   
 @app.route("/api/v1.0/stations")
@@ -50,6 +58,8 @@ def stations():
     session = Session(engine)
     sel = [Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation]
     result = session.query(*sel).all()  
+    session.close()
+    
     stations = []
     for station, name, latitude, longitude, elevation in  result:
         station_dict = {}
@@ -59,6 +69,7 @@ def stations():
         station_dict["longitude"] = longitude
         station_dict["elevation"] = elevation
         stations.append(station_dict)
+    stations = list(np.ravel(result))
     return jsonify(stations)
 
 
@@ -70,19 +81,24 @@ def temp_obs():
     latest_date = dt.datetime.strftime(latest_obs, '%Y-%m-%d') - dt.timedelta(days=365)
     sel = [Measurement.date, Measurement.tobs]
     result = session.query(*sel).all()
+    session.close()
+     
     temp_obs = []
-    for in result:
+    for date, tobs in result:
         temp_obs_dict = {}
         temp_obs_dict["date"] = date
         temp_obs_dict["Measurement.tobs"] = tobs
         temp_obs.append(temp_obs_dict)
+    temp_obs = list(np.ravel(result))
     return jsonify(temp_obs)
         
 #### API route 4/5:    
-@app.route("/api/v1.0/<start>)
+@app.route("/api/v1.0/<start>")
 def temps_from_start(start):
     session = Session(engine)
     result = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >=       start).all()
+    session.close()
+    
     temps_from_start = []
     for min, avg, max in result:
         temps_from_start_dict = {}
@@ -90,6 +106,7 @@ def temps_from_start(start):
         temps_from_start["avg"] = avg
         temps_from_start["max"] = max
         temps_from_start.append(temps_from_start_dict)        
+    temps_from_start = list(np.ravel(result))
     return jsonify(temps_from_start)
 
 #### API route 5/5:
@@ -97,6 +114,8 @@ def temps_from_start(start):
 def temps_start_to_end(start, end):
     session = Session(engine)
     result = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >=       start).filter(Measurement.date <= end).all()     
+    session.close()
+    
     temps_start_to_end = []
     for min, avg, max in result:
         temps_start_to_end_dict = {}
@@ -104,8 +123,9 @@ def temps_start_to_end(start, end):
         temps_start_to_end["avg"] = avg
         temps_start_to_end["max"] = max
         temps_start_to_end.append(temps_start_to_end_dict)
+    temps_start_to_end = list(np.ravel(result))
     return jsonify(temps_start_to_end)
            
            
 if __name__ == "__main__":
-    climate_app.run(debug=True)
+    app.run(debug=True)
